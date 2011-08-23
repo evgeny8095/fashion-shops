ec04dec84f4c099cf4513d62009c5c1830e5377e
SaRy <isaryx@gmail.com>
2011-08-23 11:10:14 +0700

    Revert 5920a7674b0d3b53b03f015dd499a45f9fc68cee^..HEAD

---
+++ ipc/BigProductSliderViewController.h
@@ -0,0 +1,16 @@
+//
+//  BigProductSliderViewController.h
+//  ipc
+//
+//  Created by SaRy on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BigProductSliderViewController : UIViewController{
+    UIScrollView *productBigSlider;
+}
+
+-(void)revealDetails:(id)sender;
+@end
