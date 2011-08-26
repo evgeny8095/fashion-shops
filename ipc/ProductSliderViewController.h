7acb842112aa1cd93b02c343429e5ad20d49593d
SaRy <isaryx@gmail.com>
2011-08-26 10:18:35 +0700

    26/8.1

--- ipc/ProductSliderViewController.h
+++ ipc/ProductSliderViewController.h
@@ -9,14 +9,19 @@
 #import <UIKit/UIKit.h>
 #import "BigProductSliderViewController.h"
 
-@interface ProductSliderViewController : UIViewController <BigProductSliderViewControllerDelegate>{
+@interface ProductSliderViewController : UIViewController <UIScrollViewDelegate, BigProductSliderViewControllerDelegate>{
     UIScrollView *productSmallSlider;
     UIScrollView *productBigSlider;
     // UIScrollView *productDetailSlider;
     NSInteger totalItem;
+    NSMutableArray *buttons;
+    UIPageControl *pageControl;
+    BOOL pageControlUsed;
+    NSArray *imageArray;
 }
 -(IBAction)swapViewSmallToBig:(id)sender;
 -(IBAction)swapViewBigToSmall:(id)sender;
 - (IBAction)gotoProductDetails:(id)sender;
+@property (nonatomic, retain) NSMutableArray *buttons;
 
 @end
--- ipc/ProductSliderViewController.m
+++ ipc/ProductSliderViewController.m
@@ -9,7 +9,13 @@
 #import "ProductSliderViewController.h"
 #import "BigProductSliderViewController.h"
 
+@interface ProductSliderViewController (PrivateMethods)
+- (void)loadScrollViewWithPage:(int)page;
+- (void)scrollViewDidScroll:(UIScrollView *)sender;
+@end
+
 @implementation ProductSliderViewController
+@synthesize buttons;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -36,7 +42,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     NSArray *productArray = [[NSArray alloc] initWithObjects:@"San Pham 1",@"San Pham 2", @"San Pham 3", @"San Pham 4", @"San Pham 5", @"San Pham 6", @"San Pham 7", @"San Pham 8", @"San Pham 9", @"San Pham 1", @"San Pham 1", @"San Pham 1", @"San Pham 1", @"San Pham 1", @"San Pham 1", @"San Pham 10", @"San Pham 11", @"San Pham 12", @"San Pham 13", @"San Pham 14", @"San Pham 15", @"San Pham 16", @"San Pham 17", @"San Pham 18", @"San Pham 19", @"San Pham 20", @"San Pham 21", @"San Pham 22", @"San Pham 23", @"San Pham 24", @"San Pham 25", @"San Pham 26", @"San Pham 27", @"San Pham 28", @"San Pham 29", @"San Pham 30", @"San Pham 31", @"San Pham 32", @"San Pham 33", @"San Pham 34", @"San Pham 35", @"San Pham 36", @"San Pham 37", @"San Pham 38", @"San Pham 39", @"San Pham 40", @"San Pham 41", @"San Pham 42", @"San Pham 43", @"San Pham 44", nil];
-    NSArray *imageArray = [[NSArray alloc] initWithObjects:@"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", nil];
+    imageArray = [[NSArray alloc] initWithObjects:@"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", @"san_pham1a.png", nil];
     
     totalItem = [productArray count];
     //[self.navigationController setDelegate:self];
@@ -48,30 +54,17 @@
     NSInteger sx = 25, bx = 0;
     NSInteger sy = 25, by = 0;
     NSInteger smallSliderWidth, bigSliderWidth;
-    for (NSInteger i = 0; i < productArray.count; i++) {
-        //image
-        UIImage *image = [UIImage imageNamed:[imageArray objectAtIndex:i]];
-        [image stretchableImageWithLeftCapWidth:0 topCapHeight:655];
-        
-        //big slider
-        //UIImageView *bigImageView = [[UIImageView alloc] initWithImage:image];
-        UIButton *bigButton = [[UIButton alloc] initWithFrame:CGRectMake(bx, by, 1024, 655)];
-        [bigButton setImage:image forState:normal];
-        //[bigButton setTitle:[productArray objectAtIndex:i] forState:normal];
-        [bigButton addTarget:self action:@selector(swapViewBigToSmall:) forControlEvents:UIControlEventTouchUpInside];
-        [productBigSlider addSubview:bigButton];
-        [bigButton release];
-        
-        bx = bx + 1024;
-        
+    
+    NSMutableArray *tempButtons = [[NSMutableArray alloc] init];
+    for (unsigned i = 0; i < totalItem; i++)
+    {
         //small slider
         UIButton *smallButton = [[UIButton alloc] initWithFrame:CGRectMake(sx, sy, 225, 290)];
-        [smallButton setImage:image forState:normal];
+        //[smallButton setImage:image forState:normal];
         [smallButton setTitle:self.navigationItem.title forState:normal];
         [smallButton setTag:i];
         [smallButton addTarget:self action:@selector(gotoProductDetails:) forControlEvents:UIControlEventTouchUpInside];
-        [productSmallSlider addSubview:smallButton];
-        [smallButton release];
+        //[productSmallSlider addSubview:smallButton];
         
         sy = sy + 25 + 290;
         if (sy > 600){
@@ -81,22 +74,30 @@
         if (i % 8 == 7) {
             sx = sx + 25;
         }
-        
-        if (i % 8 == 1) {
-            smallSliderWidth = sx + 749;
-        }  
+		[tempButtons addObject:smallButton];
+        [smallButton release];
     }
+    
+    self.buttons = tempButtons;
+    [tempButtons release];
+    for(UIButton *button in buttons){
+        [productSmallSlider addSubview:button];
+    }
+
+    smallSliderWidth = (totalItem/8+1)*1024;
     bigSliderWidth = bx;
     productSmallSlider.pagingEnabled = YES;
     productSmallSlider.contentSize = CGSizeMake(smallSliderWidth, 655);
     productSmallSlider.backgroundColor = [UIColor grayColor];
     productSmallSlider.showsHorizontalScrollIndicator = YES;
     productSmallSlider.showsVerticalScrollIndicator = NO;
+    productSmallSlider.delegate=self;
     
     productBigSlider.pagingEnabled = YES;
     productBigSlider.contentSize = CGSizeMake(bigSliderWidth, 655);
     
     [self.view addSubview:productSmallSlider];
+    [self loadScrollViewWithPage:0];
 }
 
 - (void)viewDidUnload
@@ -142,9 +143,53 @@
     [productSmallSlider scrollRectToVisible:CGRectMake(1024*page, 0, 1024, 655) animated:NO];
 }
 
-//#pragma mark navigation delegate method
-//- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
-//    [productSmallSlider scrollRectToVisible:CGRectMake(0, 0, 1024, 655) animated:NO];
-//}
+- (void)loadScrollViewWithPage:(int)page
+{
+    if (page < 0)
+        return;
+    if (page >= totalItem/8+1)
+        return;
+    NSInteger truePage = page + 1;
+    NSInteger items = truePage*8;
+    items = items > totalItem ? totalItem : items;
+    NSInteger offset = page*8;
+    for (NSInteger i = offset; i < items; i++) {
+        UIButton *button = [buttons objectAtIndex:i];
+        if ([[button imageView] image] == NULL) {
+            UIImage *image = [UIImage imageNamed:[imageArray objectAtIndex:i]];
+            [button setImage:image forState:normal];
+        }        
+    }
+}
 
+
+#pragma mark scroll controller delegate
+- (void)scrollViewDidScroll:(UIScrollView *)sender
+{
+    // We don't want a "feedback loop" between the UIPageControl and the scroll delegate in
+    // which a scroll event generated from the user hitting the page control triggers updates from
+    // the delegate method. We use a boolean to disable the delegate logic when the page control is used.
+    if (pageControlUsed)
+    {
+        // do nothing - the scroll was initiated from the page control, not the user dragging
+        return;
+    }
+	
+    // Switch the indicator when more than 50% of the previous/next page is visible
+    CGFloat pageWidth = productSmallSlider.frame.size.width;
+    int page = floor((productSmallSlider.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    pageControl.currentPage = page;
+    
+    // load the visible page and the page on either side of it (to avoid flashes when the user starts scrolling)
+    if(page!=(totalItem/8))
+    {
+        [self loadScrollViewWithPage:page - 1];
+        [self loadScrollViewWithPage:page];
+        [self loadScrollViewWithPage:page + 1];
+    }
+    
+    
+    
+    // A possible optimization would be to unload the views+controllers which are no longer visible
+}
 @end
